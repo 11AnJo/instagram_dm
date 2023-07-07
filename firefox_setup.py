01bfcd6338ba24fba658314b015831f175724c37
11AnJo <63150129+11AnJo@users.noreply.github.com>
2023-07-07 13:57:47 +0200

    udpate for selenium 4.10 ver

--- firefox_setup.py
+++ firefox_setup.py
@@ -1,24 +1,21 @@
 from selenium import webdriver
 from selenium.webdriver.firefox.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
-
 
 
 def create_firefox_driver():
+    path_to_firefox = r'C:\Program Files\Mozilla Firefox\Firefox.exe'
+
     options = Options()
     #options.add_argument('--headless')
     options.set_preference("intl.accept_languages", "en-US") # Specify the language code
-    
-    path_to_firefox = 'C:\Program Files\Mozilla Firefox\Firefox.exe'
-    binary = FirefoxBinary(path_to_firefox)
+    options.binary_location = path_to_firefox
 
-    driver = webdriver.Firefox(options=options,firefox_binary=binary)
+    driver = webdriver.Firefox(options=options)
     return driver
 
 
 def initialize_driver():
     driver = create_firefox_driver()
-    wait = WebDriverWait(driver, 10)
+    wait = WebDriverWait(driver, 5)
     return driver, wait
-
